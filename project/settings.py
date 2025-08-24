936a2fe98070dd289060bcd6998d3239cf6f0211
NicatDev <nicat254memmedov@gmail.com>
2025-08-24 13:44:06 +0400

    debug

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-86u+ann-lni!djybxc5dut!lrbb%4ud#yo#y=f+@#o0w(jx&oi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
