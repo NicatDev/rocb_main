b889c3d7fa8c9f325e83b36ec672e5d0b5975dbb
Aytac Mehdizadə <aytacmehdizade1121@gmail.com>
2025-08-26 22:51:20 +0400

    utilize-our-premises surveys

--- publicity/views.py
+++ publicity/views.py
@@ -1,3 +1,8 @@
+from .models.model_surveys import Survey
+import os
+from django.conf import settings
+from django.http import HttpResponse, Http404, FileResponse
+from django.shortcuts import render, get_object_or_404
 from .models.model_cct_center import CCTCenter, CCT_Center_Image
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
@@ -50,3 +55,35 @@ def cct_center_page(request):
         "cct_centers": cct_centers,
         "images": images,
     })
+
+
+def utilize_our_premises_page(request):
+    return render(request, "utilize_our_premises.html", {})
+
+
+def survey_page(request):
+    surveys = Survey.objects.all()
+    context = {
+        'surveys': surveys
+    }
+    return render(request, "surveys.html", context)
+
+
+def survey_pdf_view(request, survey_id):
+    survey = get_object_or_404(Survey, id=survey_id)
+
+    if not survey.file:
+        raise Http404("Survey file not found")
+
+    file_path = survey.file.path
+    if not os.path.exists(file_path):
+        raise Http404("Survey file does not exist")
+
+    try:
+        return FileResponse(
+            open(file_path, 'rb'),
+            content_type='application/pdf',
+            filename=f"{survey.title}.pdf"
+        )
+    except IOError:
+        raise Http404("Error opening survey file")
